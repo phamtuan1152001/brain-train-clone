f22147f18d85e6f198486158e0f2c169a9ea627f
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-12-06 16:45:26 +0700

    layout home page

---
+++ lib/constants/mock_data.dart
@@ -0,0 +1,32 @@
+class MockData {
+  static List<Map<String, dynamic>> cards = [
+    {
+      "id": 1,
+      "image": 'images/language-background.jpg',
+      "icon": 'images/language-icon.jpg',
+      "title": 'Trò Chơi Ngôn Ngữ',
+      "description": '4 Trò Chơi',
+    },
+    {
+      "id": 2,
+      "image": 'images/attention-background.png',
+      "icon": 'images/attention-icon.png',
+      "title": 'Trò Chơi Tập Trung',
+      "description": '4 Trò Chơi',
+    },
+    {
+      "id": 3,
+      "image": 'images/memory-background.png',
+      "icon": 'images/memory.png',
+      "title": 'Trò Chơi Ghi Nhớ',
+      "description": '4 Trò Chơi',
+    },
+    {
+      "id": 4,
+      "image": 'images/math-background.png',
+      "icon": 'images/math-icon.jpg',
+      "title": 'Trò Chơi Tính Toán',
+      "description": '4 Trò Chơi',
+    },
+  ];
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:brain_train_clone_app/constants/mock_data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // Color color = Color(0xeaddcff);
+    final cards = MockData.cards;
+    return Scaffold(
+        // appBar: AppBar(
+        //   title: Text(widget.title),
+        // ),
+        body: SafeArea(
+      child: Container(
+        width: double.infinity,
+        height: /* 900 */ double.infinity,
+        color: HexColor("#fffffe"),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                'Welcome to Brain Train',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: HexColor("#020826"), fontSize: 25, letterSpacing: 1),
+              ),
+            ),
+            for (var card in cards)
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Card(
+                  color: HexColor("#eaddcf"),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Center(
+                          child: Text(
+                            card['title'],
+                            style: TextStyle(color: HexColor("#020826")),
+                          ),
+                        ),
+                        subtitle: Center(
+                          child: Text(
+                            card['description'],
+                            style: TextStyle(color: HexColor("#716040")),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextButton(
+                            style: TextButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                // padding: const EdgeInsets.all(16.0),
+                                // textStyle: const TextStyle(fontSize: 20),
+                                backgroundColor: HexColor("#8c7851")),
+                            onPressed: () {},
+                            child: const Text('Start playing'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    ));
+  }
+}
